2b1d9f4aa1f15bea2990b9f5d1a1de9b220e37e0
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-02 18:53:46 +0100

    mise en place de la barre de navigation + burger

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,9 +1,11 @@
 import Carousel2 from "../components/Carousel2";
+import Navbar from "../components/Navbar";
 
 
 const PageHome = () => {
     return (
       <div>
+        <Navbar />
         <Carousel2 />
       </div>
     );
