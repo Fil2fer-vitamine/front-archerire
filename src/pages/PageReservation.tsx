6eec430a809d17ad45475c0d14f034fa28a72870
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-20 23:09:51 +0100

    modale constituée

--- src/pages/PageReservation.tsx
+++ src/pages/PageReservation.tsx
@@ -1,4 +1,4 @@
-import Button from "react-bootstrap/esm/Button";
+import Button from 'react-bootstrap/esm/Button';
 
 const PageReservation = () => {
   return (
@@ -36,7 +36,107 @@ const PageReservation = () => {
                   aria-label='Close'
                 ></button>
               </div>
-              <div className='modal-body'>...</div>
+              <div className='modal-body'>
+                <div className='mb-3'>
+                  <label
+                    htmlFor='exampleFormControlInput1'
+                    className='form-label'
+                  >
+                    nom :
+                  </label>
+                  <input
+                    type='text'
+                    className='form-control'
+                    id='exampleFormControlInput1'
+                    placeholder='ex : ANDRE-DE-LA-TOUR'
+                  />
+                  <label
+                    htmlFor='exampleFormControlInput1'
+                    className='form-label'
+                  >
+                    role :
+                  </label>
+                  <input
+                    type='text'
+                    className='form-control'
+                    id='exampleFormControlInput2'
+                    placeholder='ex : particulier, société, association ou autre'
+                  />
+                </div>
+                <label
+                  htmlFor='exampleFormControlInput2'
+                  className='form-label'
+                >
+                  animation souhaitée :
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='exampleFormControlInput3'
+                  placeholder='ex : particulier, société, association ou autre'
+                />
+                <label
+                  htmlFor='exampleFormControlInput3'
+                  className='form-label'
+                >
+                  lieu :
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='exampleFormControlInput4'
+                  placeholder='ex : terrain aménagé ou chez le particulier'
+                />
+                <label
+                  htmlFor='exampleFormControlInput4'
+                  className='form-label'
+                >
+                  ville :
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id='exampleFormControlInput5'
+                  placeholder='ex : Fresnoy-En-Thelle'
+                />
+                <label
+                  htmlFor='exampleFormControlInput5'
+                  className='form-label'
+                >
+                  nombre de participants :
+                </label>
+                <input
+                  type='number'
+                  className='form-control'
+                  id='exampleFormControlInput6'
+                  placeholder='ex : 12'
+                />
+                <label
+                  htmlFor='exampleFormControlInput6'
+                  className='form-label'
+                >
+                  date :
+                </label>
+                <input
+                  type='date'
+                  className='form-control'
+                  id='exampleFormControlInput7'
+                />
+
+                <div className='mb-3'>
+                  <label
+                    htmlFor='exampleFormControlTextarea7'
+                    className='form-label'
+                  >
+                    Notes
+                  </label>
+                  <textarea
+                    className='form-control'
+                    id='exampleFormControlTextarea1'
+                    rows={3}
+                  ></textarea>
+                </div>
+              </div>
               <div className='modal-footer'>
                 <button
                   type='button'
