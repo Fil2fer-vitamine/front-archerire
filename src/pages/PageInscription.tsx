095fe04021e37e6354408eecc4daf448abe1c62f
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-25 16:05:31 +0100

    Page Inscription fonctionnelle

--- src/pages/PageInscription.tsx
+++ src/pages/PageInscription.tsx
@@ -3,7 +3,20 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import BoutonAnnuler from '../components/BoutonAnnuler';
 
+// Gestion de la page d'inscription
 const PageInscription = () => {
+  /**
+   * Utilisation de useRef() pour créer une référence à neuf éléments du DOM :
+   * nameElement : pour le stockage de la saisie du nom par l'utilisateur.
+   * firstnameElement : pour le stockage de la saisie du prénom par l'utilisateur.
+   * adressElement : pour le stockage de la saisie de l'adresse par l'utilisateur.
+   * postal_codeElement : pour le stockage de la saisie du code postal par l'utilisateur.
+   * cityElement : pour le stockage de la saisie de la ville par l'utilisateur.
+   * phoneElement : pour le stockage de la saisie du numéro de téléphone par l'utilisateur.
+   * emailElement : pour le stockage de la saisie de l'email par l'utilisateur.
+   * passwordElement : pour le stockage de la saisie du password par l'utilisateur.
+   * confirmPasswordElement : pour le stockage de la saisie de la confirmation du password par l'utilisateur.
+   */
   const nameElement = useRef<HTMLInputElement>(null);
   const firstnameElement = useRef<HTMLInputElement>(null);
   const adressElement = useRef<HTMLInputElement>(null);
@@ -12,6 +25,12 @@ const PageInscription = () => {
   const phoneElement = useRef<HTMLInputElement>(null);
   const emailElement = useRef<HTMLInputElement>(null);
   const passwordElement = useRef<HTMLInputElement>(null);
+  const confirmPasswordElement = useRef<HTMLInputElement>(null);
+
+  /**
+   * A la suite des saisies, on souhaite permettre une navigation à un moment donné :
+   * variable navigate créée pour une utilisation quand l'inscription est effective
+   */
   const navigate = useNavigate();
 
   const handleSubmitForm = (e: FormEvent) => {
@@ -21,41 +40,46 @@ const PageInscription = () => {
       '---emailElement.current?.value--- -->',
       emailElement.current?.value
     );
-    console.log(
-      '---passwordElement.current?.value--- -->',
-      passwordElement.current?.value
-    );
 
     // Gestion des erreurs de saisies que pourrait faire l'utilisateur avnt d'entrer dans l'axios : tous les champs sont obligatoires.
     if (nameElement.current?.value === '') {
-      return 'Tous les champs doivent être remplis';
+      alert("Le champs 'nom' doit impérativement être remplis");
     }
     if (firstnameElement.current?.value === '') {
-      return 'Tous les champs doivent être remplis ';
+      alert("Le champs 'prénom' doit impérativement être remplis");
     }
     if (adressElement.current?.value === '') {
-      return 'Tous les champs doivent être remplis';
+      alert("Le champs 'adresse' doit impérativement être remplis");
     }
     if (postal_codeElement.current?.value === '') {
-      return 'Tous les champs doivent être remplis ';
+      alert("Le champs 'code postal' doit impérativement être remplis");
     }
     if (cityElement.current?.value === '') {
-      return 'Tous les champs doivent être remplis';
+      alert("Le champs 'ville ou village' doit impérativement être remplis");
     }
     if (phoneElement.current?.value === '') {
-      return 'Tous les champs doivent être remplis ';
+      alert("Le champs 'numéro de téléphone' doit impérativement être remplis");
     }
 
     if (emailElement.current?.value === '') {
-      return 'Tous les champs doivent être remplis';
+      alert("Le champs 'email' doit impérativement être remplis");
     }
     if (passwordElement.current?.value === '') {
-      return 'Tous les champs doivent être remplis ';
+      alert("Le champs 'password' doit impérativement être remplis");
     }
 
-    axios
-      // récupération de la saisie des données pour un enregistrement
-      .post(`http://localhost:8080/api/auth/register`, {
+    /**
+     * Gestion Erreur :
+     * Si le password est différent de la confirmation du password.
+     */
+    if (
+      passwordElement.current?.value !== confirmPasswordElement.current?.value
+    ) {
+      alert('Il y a discordance au niveau de votre mot de Passe !');
+    } else {
+      const passWordVerif = passwordElement.current?.value;
+
+      let Customer = {
         name: nameElement.current?.value,
         firstname: firstnameElement.current?.value,
         adress: adressElement.current?.value,
@@ -63,16 +87,33 @@ const PageInscription = () => {
         city: cityElement.current?.value,
         phone: phoneElement.current?.value,
         email: emailElement.current?.value,
-        password: passwordElement.current?.value,
-      })
-      .then((response) => {
-        console.log(response);
-        console.log(response.data);
-        navigate('/PageConnexion');
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        password: passWordVerif,
+      };
+
+      console.log('---passWordVerif --- -->', passWordVerif);
+
+      axios
+        .post('http://localhost:8080/api/auth/register', Customer)
+        /**
+         * La variable ' Customer' prend en compte la saisie de 9 éléments
+         * De par la requête POST, on soumet Customer pour l'inscription.
+         */
+        .then((response) => {
+          /**
+           * Alors on obtiendra une réponse...
+           */
+          console.log('Réponse : ', response);
+          console.log('Data de la réponse : ', response.data);
+          alert('Inscription réussie! Passons à votre enregistrement...');
+          navigate('/PageConnexion');
+        })
+        .catch((err) => {
+          console.log("L'erreur est : ", err);
+          alert(
+            "Veuillez bien respecter la mise en forme dans chacun des champs de saisie, s'il vous plait."
+          );
+        });
+    }
   };
 
   return (
@@ -97,6 +138,7 @@ const PageInscription = () => {
                 placeholder='ex :     ANDRE-DE-LA-TOUR'
                 aria-label="Recipient's username"
                 aria-describedby='basic-addon2'
+                //  ref permet de mettre a jour mon champs
                 ref={nameElement}
                 title="Ce champ est obligatoire - Un nom se doit d'être saisi en lettres majuscules et accepte un ou plusieurs tirets."
               />
@@ -133,6 +175,7 @@ const PageInscription = () => {
                 placeholder="ex :  1, rue de l'impasse qui mène quand même vers quelquechose"
                 aria-label="Recipient's useradress"
                 aria-describedby='basic-addon2'
+                //  ref permet de mettre a jour mon champs
                 ref={adressElement}
                 title="Ce champ est obligatoire - Votre indication de rue doit être un nombre suivi d'une virgule, puis d'une chaîne de caractères en lettres majuscules ou minuscules - Ce champ accepte un ou plusieurs tirets."
               />
@@ -151,6 +194,7 @@ const PageInscription = () => {
                 placeholder='ex :     60530'
                 aria-label="Recipient's userpostal_code"
                 aria-describedby='basic-addon2'
+                //  ref permet de mettre a jour mon champs
                 ref={postal_codeElement}
                 title='Ce champ est obligatoire - Le code postal contient impérativement cinq chiffres.'
               />
@@ -169,6 +213,7 @@ const PageInscription = () => {
                 placeholder='ex :     Fresnoy-En-Thelle'
                 aria-label="Recipient's usercity"
                 aria-describedby='basic-addon2'
+                //  ref permet de mettre a jour mon champs
                 ref={cityElement}
                 title='Ce champ est obligatoire - Le champ doit commencer par une lettre majuscule suivie de lettres minuscules et accepte un ou plusieurs tirets.'
               />
@@ -187,6 +232,7 @@ const PageInscription = () => {
                 placeholder='ex :     0612345678'
                 aria-label="Recipient's userphone"
                 aria-describedby='basic-addon2'
+                //  ref permet de mettre a jour mon champs
                 ref={phoneElement}
                 title='Ce champ est obligatoire - Votre numéro de téléphone ne doit être composé que de dix chiffres.'
               />
@@ -207,6 +253,7 @@ const PageInscription = () => {
                 placeholder='ex :     philippe-de-la-tour@archerire.fr'
                 aria-label="Recipient's useremail"
                 aria-describedby='basic-addon2'
+                //  ref permet de mettre a jour mon champs
                 ref={emailElement}
                 title="Votre adresse email nous servira d'identifiant - Ce champ est obligatoire"
               />
@@ -224,20 +271,40 @@ const PageInscription = () => {
                 placeholder='(Obligatoire)'
                 aria-label="Recipient's userpassword"
                 aria-describedby='basic-addon2'
+                //  ref permet de mettre a jour mon champs
                 ref={passwordElement}
                 title='Un bon mot de passe doit comporter au moins 8 lettres, dont au moins une Majuscule et/ou une minuscule - Ce champ est obligatoire'
               />
 
-              <div>
-                <BoutonAnnuler />{' '}
-                <button
-                  type='button'
-                  className='btn btn-outline-success btn-lg'
-                  onClick={handleSubmitForm}
-                >
-                  Valider
-                </button>
+              <div className='input-group mb-3'>
+                <span className='input-group-text' id='basic-addon2'>
+                  <pre>
+                    Veuillez confirmer votre <strong>mot de passe</strong>
+                  </pre>
+                </span>
+                <input
+                  type='text'
+                  // type='password' à mettre pour coder le mot de passe
+                  className='form-control'
+                  placeholder='(Obligatoire)'
+                  aria-label="Recipient's userpassword"
+                  aria-describedby='basic-addon2'
+                  //  ref permet de mettre a jour mon champs
+                  ref={confirmPasswordElement}
+                  title='Un bon mot de passe doit comporter au moins 8 lettres, dont au moins une Majuscule et/ou une minuscule - Ce champ est obligatoire'
+                />
               </div>
+            </div>
+
+            <div>
+              <BoutonAnnuler />{' '}
+              <button
+                type='button'
+                className='btn btn-outline-success btn-lg'
+                onClick={handleSubmitForm}
+              >
+                Valider
+              </button>
             </div>
           </div>
         </div>
