88761b3eda4755963c2673c16715859326f24871
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-25 18:50:49 +0100

    Essai de mise de photos issues de l'ancien site

--- src/pages/PagePhoto.tsx
+++ src/pages/PagePhoto.tsx
@@ -1,9 +1,55 @@
 const PagePhoto = () => {
-    return (
-        <div>
-
+  return (
+    <div>
+      <section>
+        <div className='card'>
+          <div className='info'>
+            <h1 className='title'>Arc</h1>
+            <p className='description'>
+              Les arcs que nous utilisons sont constitués en polymère
+              garantissant une résistance à toute épreuve.
+            </p>
+          </div>
         </div>
-    );
+        <div className='card1'>
+          <div className='info'>
+            <h1 className='title'>Flèche</h1>
+            <p className='description'>
+              Nos flèches comportent, en terme de protection, des embouts en
+              mousse.
+              <br />
+              Le corps de ces flèches sont en fibre de verre assurant une bonne
+              légèreté et résistance
+            </p>
+          </div>
+        </div>
+        <div className='card2'>
+          <div className='info'>
+            <h1 className='title'>Masque</h1>
+            <p className='description'>
+              Tout comme les arcs que nous utilisons, nos masques sont
+              constitués en polymère garantissant une résistance à toute
+              épreuve.
+              <br />
+              Ces masques sont désaffectés à chaque utilisation.
+            </p>
+          </div>
+        </div>
+        <div className='card3'>
+          <div className='info'>
+            <h1 className='title'>Bunker</h1>
+            <p className='description'>
+              Nos structures gonflables sont aménagées sur le terrain de façon à
+              organiser la défense.
+              <br />
+              Ils sont attachés ingénieusement au sol, quelque soit le type de
+              terrain, de façon à ne pas entraver la circulation des joueurs.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default PagePhoto;
