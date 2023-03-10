376e45cb71e88761a6849815fdf407c863492cd1
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-03-10 18:30:59 +0100

    photos mises dans des cartes plutôt que dans un carousel

--- src/components/CarouselInformation.tsx
+++ src/components/CarouselInformation.tsx
@@ -1,45 +1,104 @@
 const Carousel2 = () => {
   return (
-    <div className='centrage'>
-      <div className='w-25 bg-info'>
+    <div className=''>
+      <div className=' '>
         {/* Cette div nous permet d'obtenir un carousel de petite dimension.
-      w-50 : l'élément a une largeur égale à 50% de celle de son parent ;*/}
+      w-25 : l'élément a une largeur égale à 50% de celle de son parent ;*/}
 
-        <div
+        {/* <div
           id='carouselExampleInterval'
           className='carousel slide'
           data-bs-ride='carousel'
-        >
-          {/* Constitution d'un carousel avec interval de 2 secondes entre les images */}
-          {/* Présentation de la première photo */}
-          <div className='carousel-inner'>
-            <div className='carousel-item active' data-bs-interval='3000'>
-              <img
-                src='./Assets/ImagesCarousel/IMG_0611.JPG'
-                className='d-block w-100'
-                alt='Deux archers en action'
-              />
-            </div>
+        > */}
+        {/* Constitution d'un carousel avec interval de 2 secondes entre les images */}
+        {/* Présentation de la première photo */}
+        <div className='' style={{ display: 'flex', width: '100%'  }}>
+          
+          <div>
+            <img
+              src='./Assets/ImagePanoplie/bunker.png'
+              className=' w-100'
+              alt='La flêche est une des accessoire.'
+            />
+          </div>
+          <div>
+            <img
+              src='./Assets/ImagePanoplie/fleches.png'
+              className=' w-100'
+              alt='La flêche est une des accessoire.'
+            />
+          </div>
 
-            {/* Présentation de la deuxième photo */}
-            <div className='carousel-item' data-bs-interval='3000'>
-              <img
-                src='./Assets/ImagesCarousel/IMG_0629.JPG'
-                className='d-block w-100'
-                alt='Un groupe archers'
-              />
-            </div>
+          <div>
+            <img
+              src='./Assets/ImagePanoplie/un-arcredimension.png'
+              className=' w-100'
+              alt='Le masque de protection est un des accessoire.'
+            />
+          </div>
 
-            {/* Présentation de la troisième photo */}
-            <div className='carousel-item' data-bs-interval='3000'>
+          <div>
+            <img
+              src='./Assets/ImagePanoplie/lemasque.png'
+              className=' w-100'
+              alt='Le masque de protection est un des accessoire.'
+            />
+          </div>
+
+          
+
+          {/* <div
+              className='carousel-item active'
+              data-bs-interval='3000'
+              style={{ height: '330px', backgroundColor: 'white' }}
+            >
               <img
-                src='./Assets/ImagesCarousel/IMG_0644.JPG'
+                src='./Assets/ImagePanoplie/flechesredimension.png'
+                className='d-block w-100'
+                alt='La flêche est une des accessoire.'
+              />
+            </div> */}
+
+          {/* Présentation de la deuxième photo */}
+          {/* <div
+              className='carousel-item'
+              data-bs-interval='3000'
+              style={{ height: '330px', backgroundColor: 'white' }}
+            >
+              <img
+                src='./Assets/ImagePanoplie/lemasqueredimension.png'
+                className='d-block w-100'
+                alt='Le masque de protection est un des accessoire.'
+              />
+            </div> */}
+
+          {/* Présentation de la troisième photo */}
+          {/* <div
+              className='carousel-item'
+              data-bs-interval='3000'
+              style={{ height: '330px', backgroundColor: 'white' }}
+            >
+              <img
+                src='./Assets/ImagePanoplie/un-arcredimension.png'
                 className='d-block w-100'
                 alt='Un groupe en protection'
               />
-            </div>
-          </div>
-          <button
+            </div> */}
+
+          {/* Présentation de la quatrième photo */}
+          {/* <div
+              className='carousel-item'
+              data-bs-interval='3000'
+              style={{ height: '330px', backgroundColor: 'white' }}
+            >
+              <img
+                src='./Assets/ImagePanoplie/bunkerredimension.png'
+                className='d-block w-100'
+                alt='Un groupe en protection'
+              />
+            </div> */}
+          {/* </div> */}
+          {/* <button
             className='carousel-control-prev'
             type='button'
             data-bs-target='#carouselExampleInterval'
@@ -50,8 +109,8 @@ const Carousel2 = () => {
               aria-hidden='true'
             ></span>
             <span className='visually-hidden'>Previous</span>
-          </button>
-          <button
+          </button> */}
+          {/* <button
             className='carousel-control-next'
             type='button'
             data-bs-target='#carouselExampleInterval'
@@ -62,7 +121,7 @@ const Carousel2 = () => {
               aria-hidden='true'
             ></span>
             <span className='visually-hidden'>Next</span>
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
--- src/components/ReseauxSociaux.tsx
+++ src/components/ReseauxSociaux.tsx
@@ -1,22 +1,26 @@
+import "./ReseauxSociaux.css";
+
 const ReseauxSociaux = () => {
   return (
     <div className='reseaux-sociaux-container'>
-      <li>
-        <a
-          href='https://www.facebook.com/profile.php?id=100080990201868'
-          target='blank'
-        >
-          <i className='fa-brands fa-square-facebook fa-2xl'></i>
-        </a>
-      </li>
-      <li>
-        <a
-          href='https://www.instagram.com/archerire.battle/?next=%2F'
-          target='blank'
-        >
-          <i className='fa-brands fa-instagram fa-2xl'></i>
-        </a>
-      </li>
+      <ul>
+        <li>
+          <a
+            href='https://www.facebook.com/profile.php?id=100080990201868'
+            target='blank'
+          >
+            <i className='fa-brands fa-square-facebook fa-2xl'></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.instagram.com/archerire.battle/?next=%2F'
+            target='blank'
+          >
+            <i className='fa-brands fa-instagram fa-2xl'></i>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
